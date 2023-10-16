a6c6af87021866825f994afc77789918052f6383
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-10-16 10:05:17 -0300

    style: add global style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        grey: {
+          100: "#F2F2F2",
+          200: "#D9D9D9",
+          300: "#808080",
+          400: "#333333",
+          500: "#262626",
+          600: "#1A1A1A",
+          700: "#0D0D0D",
+        },
+        blue: {
+          200: "#4EA8DE",
+          500: "#1E6F9F",
+        },
+        purple: {
+          200: "#8284FA",
+          500: "#5E60CE",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
